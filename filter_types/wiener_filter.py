7a00d28b2250fc968dfe75eb93ed08cb77f5d37d
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-04-27 17:12:17 +0200

    last updates. consolidated code.

--- filter_types/wiener_filter.py
+++ filter_types/wiener_filter.py
@@ -12,7 +12,8 @@ import math
 import random
 import soundfile as sf
 
-import matplotlib.pyplot as plt
+
+#import matplotlib.pyplot as plt
 import scipy.signal as sig
 
 
@@ -32,80 +33,33 @@ def match_lengths(target, noise):
                 noise.append(noise[i])
             noise = np.array(noise)
     return target, noise
-       
+
 
 
 # orignal signal
-s_wav = "./German-speech-sample-female.wav"
-s,sr = librosa.load(s_wav, sr= 16000,duration=2)
+s_wav = "//home/airos/Data/sound/aislyn_audacity_reducednoise.wav"
+s,sr = librosa.load(s_wav, sr= 16000,duration=10)
 # noise samples (make same length as recorded sample)
-n_wav = "./bus.wav"
+n_wav = "//home/airos/Data/sound/noise_keyboard.wav"
 n, sr = librosa.load(n_wav, sr=16000,duration=2)
 
 s, n = match_lengths(s,n)
-
-
-# original signal with noise
 x = s+n
 
-#stft of all
-s_stft = librosa.stft(s)
-n_stft = librosa.stft(n)
-x_stft = librosa.stft(x)
+f, Pxx = sig.csd(x,x,fs=16000,scaling='density')
+f, Psx = sig.csd(s,x,fs=16000,scaling='density')
 
-s_pow = np.abs(s_stft)**2
-x_pow = np.abs(x_stft)**2
-
-
-N = len(x)
-M = 256  # length of Wiener filter
-
-# estimate (cross) PSDs using Welch technique
-f, Pxx = sig.csd(x_pow, x_pow)
-f, Psx = sig.csd(s_pow, x_pow)
-# compute Wiener filter
+#calculate Wiener filter
 H = Psx/Pxx
 H = H * np.exp(-1j*2*np.pi/len(H)*np.arange(len(H))*(len(H)//2))  # shift for causal filter
 h = np.fft.irfft(H)
-# apply Wiener filter to observation
-y = np.convolve(x, h, mode='same')
+#apply Wiener filter to observation
+y = np.convolve(x,h, mode='same')
 
-# plot (cross) PSDs
-Om = np.linspace(0, np.pi, num=len(H))
+print(y.shape)
+
+ 
 
 
-
-plt.figure(figsize=(10, 4))
-plt.subplot(121)
-plt.plot(Om, 20*np.log10(np.abs(.5*Pxx)), label=r'$| \Phi_{xx}(e^{j \Omega}) |$ in dB')
-plt.plot(Om, 20*np.log10(np.abs(.5*Psx)), label=r'$| \Phi_{sx}(e^{j \Omega}) |$ in dB')
-plt.title('(Cross) PSDs')
-plt.xlabel(r'$\Omega$')
-plt.legend()
-plt.axis([0, np.pi, -60, 40])
-plt.grid()
-
-# plot transfer function of Wiener filter
-plt.subplot(122)
-plt.plot(Om, 20*np.log10(np.abs(H)))
-plt.title('Transfer function of Wiener filter')
-plt.xlabel(r'$\Omega$')
-plt.ylabel(r'$| H(e^{j \Omega}) |$ in dB')
-plt.axis([0, np.pi, -150, 3])
-plt.grid()
-plt.tight_layout()
-
-# plot signals
-idx = np.arange(500, 600)
-plt.figure(figsize=(10, 4))
-plt.plot(idx, x[idx], label=r'observed signal $x[k]$')
-plt.plot(idx, s[idx], label=r'original signal $s[k]$')
-plt.plot(idx, y[idx], label=r'estimated signal $y[k]$')
-plt.title('Signals')
-plt.xlabel(r'$k$')
-plt.axis([idx[0], idx[-1], -1.5, 1.5])
-plt.legend()
-plt.grid()
-plt.show()
-
-sf.write("wiener_test.wav",y,1600)
+sf.write("wiener_test_n_nr.wav",y,16000)
+sf.write("wiener_test_n.wav",x,16000)
